702c6a5620ecd079287706750c82a5fd166c3d75
Nannita1403 <nannimagi@gmail.com>
2024-04-15 13:39:28 +0200

    ok

--- P5-Games/public/src/pages/wellcome/wellcome.js
+++ P5-Games/public/src/pages/wellcome/wellcome.js
@@ -1,14 +1,28 @@
+import { GAMES } from '../../utils/data'
 import './wellcome.css'
+
 export const wellcome = () => {
   const wellcomeContainer = document.createElement('div')
   wellcomeContainer.className = 'container'
   const titleWellcome = document.createElement('h2')
   titleWellcome.id = 'titleWellcome'
   titleWellcome.innerHTML = 'Vamos a Jugar!'
-  //const bottonPlay = document.createElement('button')
-  //bottonPlay.id = 'bottonPlay'
-  //bottonPlay.innerText = 'PLAY'
-  wellcomeContainer.append(titleWellcome)//bottonPlay)
+
+  const gamesNav = document.createElement('nav')
+  gamesNav.className = 'nav'
+  GAMES.forEach((game) => {
+    const anchor = document.createElement('a')
+    anchor.className = 'anchor'
+    anchor.href = game.id
+    const gameButton = document.createElement('button')
+    gameButton.className = 'game-btn'
+    gameButton.id = game.name
+    gameButton.innerText = game.name
+    anchor.append(gameButton)
+    gamesNav.append(anchor)
+  })
+
+  wellcomeContainer.append(titleWellcome, gamesNav)
   const app = document.querySelector('#app')
   app.append(wellcomeContainer)
 }
